cb93d5510d47875e2ee9c5d57519760859a6dbe5
Jiayi Nie <nie.jiay@northeastern.edu>
2022-09-30 19:53:30 -0700

    add menu components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "dinnerware",
+      imageUrl: "https://i.ibb.co/wQtmqvg/dinnerware.jpg",
+    },
+    {
+      id: 2,
+      title: "drinkware",
+      imageUrl: "https://i.ibb.co/92Rxykf/drinkware.jpg",
+    },
+    {
+      id: 3,
+      title: "flatware",
+      imageUrl: "https://i.ibb.co/6y68nCM/flatware.jpg",
+    },
+    {
+      id: 4,
+      title: "serveware",
+      imageUrl: "https://i.ibb.co/7bSf3pH/serveware.jpg",
+    },
+    {
+      id: 5,
+      title: "bakeware",
+      imageUrl: "https://i.ibb.co/HtQ5m6p/bakeware.jpg",
+    },
+  ];
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
